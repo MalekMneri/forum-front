531fb2752fac2cc07df385ff0f0c4d32af9f972d
Malek Mneri <mneri.malek@gmail.com>
2022-08-30 14:08:04 +0100

    show orders and calculate lot

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -11,6 +11,59 @@ import { Event } from '../Models/Event';
 export class EventsComponent implements OnInit {
   searchTerm = '';
   events: Event[] = [];
+  orders = [
+    {
+      orderNumber: '564',
+      currencyPair: 'AUDJPY',
+      orderType: 'pendingOrder',
+      direction: 'BUY STOP',
+      condition: 'over',
+      conditionValue1: 220,
+      conditionValue2: null,
+      price: 120,
+      SL: 100,
+      TP: 100,
+      lot: 0,
+    },
+    {
+      orderNumber: '564',
+      currencyPair: 'AUDJPY',
+      orderType: 'marketExecution',
+      direction: 'BUY',
+      condition: 'between',
+      conditionValue1: 220,
+      conditionValue2: 130,
+      price: 120,
+      SL: 100,
+      TP: 100,
+      lot: 0,
+    },
+    {
+      orderNumber: '564',
+      currencyPair: 'AUDJPY',
+      orderType: 'pendingOrder',
+      direction: 'BUY STOP',
+      condition: 'over',
+      conditionValue1: 2,
+      conditionValue2: null,
+      price: 120,
+      SL: 100,
+      TP: 100,
+      lot: 0,
+    },
+    {
+      orderNumber: '564',
+      currencyPair: 'AUDJPY',
+      orderType: 'pendingOrder',
+      direction: 'BUY STOP',
+      condition: 'over',
+      conditionValue1: 2,
+      conditionValue2: null,
+      price: 120,
+      SL: 100,
+      TP: 100,
+    },
+  ];
   constructor(private eventService: EventsService) {}
 
   ngOnInit(): void {
@@ -36,5 +89,12 @@ export class EventsComponent implements OnInit {
       console.log(data);
       this.events = data;
     });
+  }
+  calculateLot(order: any, form: NgForm) {
+    if (['BUY', 'BUY STOP', 'BUY LIMIT'].includes(order.direction))
+      order.lot =
+        (form.value.capital * form.value.percentage) /
+        100 /
+        (order.price - order.SL);
   }
 }
