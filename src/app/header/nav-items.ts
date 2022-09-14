8a04cc22b8a55dcd12771e3a83a4f6054db43fb0
Malek Mneri <mneri.malek@gmail.com>
2022-09-14 23:55:31 +0100

    improve currencies interface

--- src/app/header/nav-items.ts
+++ src/app/header/nav-items.ts
@@ -2,5 +2,10 @@ export const navItems = [
   { link: 'create-order', name: 'New order', users: ['TA', 'admin'] },
   { link: 'my-orders', name: 'My Orders', users: ['TA', 'admin'] },
   { link: 'pending-orders', name: 'Pending orders', users: ['Validator'] },
-  { link: 'currencies', name: 'Currencies', users: ['Validator', 'TA', 'FA'] },
+  { link: 'currencies', name: 'Currencies', users: ['FA'] },
+  {
+    link: 'recommendations',
+    name: 'Recommendations',
+    users: ['Validator', 'TA', 'FA'],
+  },
 ];
